20f1c83b796594875d6bcf607dfac5ccd7b1b95b
Shubham Chauhan <cshubham@In-cshubham.local>
2018-12-04 15:48:19 +0530

    Introduced extractHeadArgs() and written test for that.

--- src/headLib.js
+++ src/headLib.js
@@ -11,4 +11,30 @@ const getHead = function(header, InputData) {
   return result.join("");
 }
 
+const extractHeadOption = function(args) {
+  let option = args[0].split('');
+  let result = [{files:[]}];
+  if(option[0] == "-" && !(+option[1])) {
+    result[0][option[1]] = option.slice(2).join('') || args[1];
+    option[2] || args.splice(0,1);
+    args = args.slice(1);
+    result.push(args);
+    return result;
+  }
+  result[0]["n"] = +option.slice(1).join('') || 10;
+  result.push(args);
+  return result;
+}
+
+const extractHeadArgs = function(args) {
+  let userArgs = args.slice(2);
+  let extractOption = extractHeadOption(userArgs);
+  let result = extractOption[0];
+  userArgs = extractOption[1];
+  +userArgs[0] && userArgs.shift();
+  result["files"] = userArgs;
+  return result;
+}
+
 exports.getHead = getHead;
+exports.extractHeadArgs = extractHeadArgs;
--- test/libTest.js
+++ test/libTest.js
@@ -1,28 +1,52 @@
 const assert = require('assert');
-const { getHead } = require('../src/headLib.js');
+const { getHead, extractHeadArgs } = require('../src/headLib.js');
 
 const getString = function(string){
   return string;
 };
 
-describe('', function() {
+describe('getHead', function() {
   let string;
   beforeEach('',function(){
-    string = "The coins entered circulation\n";
-    string += "After legal maneuvering";
-    string += "the government";
-    string += "The coins were";
-    string += "Congress called in the coins";
-    string += "After legal maneuvering";
-    string += "the government";
-    string += "The coins were";
-    string += "Congress called in the coins";
-    string += "The coins were";
-    string += "Congress called in the coins";
-    string += "replacing them with US currency";
+    string = "The coins entered circulation\nAfter legal maneuvering\nthe government\nThe coins were\nCongress called in the coins\nAfter legal maneuvering\nthe government\nThe coins were\nCongress called in the coins\nThe coins were\nCongress called in the coins\nreplacing them with US currency";
   });
 
   it('should return the same string which is given as input', function() {
     assert.deepEqual(getHead(getString,{file:[string]}),string);
+  });
+});
+
+
+describe("extractHeadArgs", function() {
+  describe("for array holding -n[numberOfLines] as 2 index", function() {
+    it("should return an object with n as key holding number of lines and files key holding a list of strings", function() {
+      let array = [,,"-n10","file1","file2"];
+      let result = { files: [ 'file1', 'file2' ], n: '10' }; 
+      assert.deepEqual(extractHeadArgs(array), result);
+    });
+  });
+
+  describe("for array holding -c[numberOfbytes] as 2 index", function() {
+    it("should return an object with c as key holding number of bytes and files key holding a list of strings", function() {
+      let array = [,,"-c10","file1","file2"];
+      let result = { files: [ 'file1', 'file2' ], c: '10' }; 
+      assert.deepEqual(extractHeadArgs(array), result);
+    });
+  });
+
+  describe("for array holding only number as 2 index", function() {
+    it("should return an object with n as key holding that number and files key holding a list of strings", function() {
+      let array = [,,"-10","file1","file2","file3"];
+      let result = { files: [ 'file1', 'file2', 'file3' ], n: '10' }; 
+      assert.deepEqual(extractHeadArgs(array), result);
+    });
+  });
+
+  describe("for array holding non of above as 2 index", function() {
+    it("should return an object with n as key holding 10 and files key holding a list of strings", function() {
+      let array = [,,"file1","file2","file3"];
+      let result = { files: [ 'file1', 'file2', 'file3' ], n: '10' }; 
+      assert.deepEqual(extractHeadArgs(array), result);
+    });
   });
 });
