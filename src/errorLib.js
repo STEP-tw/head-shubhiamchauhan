2255256a3320374bf63f51f9930f6dbf544cddb5
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2018-12-14 22:16:07 +0530

    Extracted logics in separte functions in errorLib.js.

--- src/errorLib.js
+++ src/errorLib.js
@@ -1,27 +1,47 @@
-const countError = { head:{ "n": "line count", "c": "byte count" },
-  tail:{ "n":"offset", "c":"offset"} };
+const countError = {
+  head: { "n": "line count", "c": "byte count" },
+  tail: { "n": "offset", "c": "offset" }
+};
 
-const usage = { head:"usage: head [-n lines | -c bytes] [file ...]",
-  tail:"usage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]"};
+const usage = {
+  head: "usage: head [-n lines | -c bytes] [file ...]",
+  tail: "usage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]"
+};
 
 const undefinedArgError = ": option requires an argument -- ";
 
-const findOptionError = function(args, command) {
+const isValidOption = function (optionType) {
+  return ["n", "c"].includes(optionType);
+}
+
+const hasNoCount = function(count){
+  return count == undefined;
+}
+
+const hasAlphanumericCount = function(count) {
+  return parseInt(count) != count;
+}
+
+const isHeadCountLessThanOne = function(count, command) {
+return count < 1 && command == "head";
+}
+
+const findOptionError = function (args, command) {
   let type = Object.keys(args)[1];
 
-  if (!["n", "c"].includes(type)) {
+  if (!isValidOption(type)) {
     return command + ": illegal option -- " + type + "\n" + usage[command];
   }
 
-  if (args[type] == undefined) {
+  if (hasNoCount(args[type])) {
     return command + undefinedArgError + type + "\n" + usage[command];
   }
 
-  if (parseInt(args[type]) != args[type]) {
+  if (hasAlphanumericCount(args[type])) {
     return command + ": illegal " + countError[command][type] + " -- " + args[type];
   }
 
-  if(args[type] < 1 && command == "head") {
+  if (isHeadCountLessThanOne(args[type], command)) {
     return "head: illegal " + countError[command][type] + " -- " + args[type];
   }
   return "";
