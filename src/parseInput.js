7718dfc98bec734e0f11e66ec8af185791a80dda
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2018-12-18 13:06:31 +0530

    Removed unnecessary code.

--- src/parseInput.js
+++ src/parseInput.js
@@ -1,29 +1,21 @@
-const extractUserOptions = function (args) {
+const extractUserArgs = function (args) {
   let option = args[0];
   if (!option.startsWith("-")) {
-    return [{ files: [], option:"n", count:10 }, args];
+    return { files: args, option: "n", count: 10 };
   }
 
   if (option[1] == "-") {
-    return [{ files: [], option:"n", count: 10 }, args.slice(1)];
+    return { files: args.slice(1), option: "n", count: 10 };
   }
 
   if (!isFinite(option.substr(1)) && +option.substr(2)) {
-    return [{ files: [], option:option[1], count: +option.substr(2) }, args.slice(1)];
+    return { files: args.slice(1), option: option[1], count: +option.substr(2) };
   }
 
   if (isFinite(parseInt(option[1]))) {
-    return [{ files: [], option:"n", count: option.substr(1) }, args.slice(1)];
+    return { files: args.slice(1), option: "n", count: option.substr(1) };
   }
-  return [{ files: [], option:option[1], count: +args[1] }, args.slice(2)];
+  return { files: args.slice(2), option: option[1], count: +args[1] };
 };
 
-const extractUserArgs = function (args) {
-  let extractOption = extractUserOptions(args);
-  let validatedArgs = extractOption[0];
-  userArgs = extractOption[1];
-  validatedArgs["files"] = userArgs;
-  return validatedArgs;
-
-};
 exports.extractUserArgs = extractUserArgs;
