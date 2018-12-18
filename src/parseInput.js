23a1b905a8155ac9f9b7ff900049f889fc691f2f
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2018-12-18 15:08:45 +0530

    Renamed option --> firstUserArg and run prettier.

--- src/parseInput.js
+++ src/parseInput.js
@@ -1,21 +1,25 @@
 const extractUserArgs = function (args) {
-  let option = args[0];
-  if (!option.startsWith("-")) {
+  let firstUserArg = args[0];
+  if (!firstUserArg.startsWith("-")) {
     return { files: args, option: "n", count: 10 };
   }
 
-  if (option[1] == "-") {
+  if (firstUserArg[1] == "-") {
     return { files: args.slice(1), option: "n", count: 10 };
   }
 
-  if (!isFinite(option.substr(1)) && +option.substr(2)) {
-    return { files: args.slice(1), option: option[1], count: +option.substr(2) };
+  if (!isFinite(firstUserArg.substr(1)) && +firstUserArg.substr(2)) {
+    return {
+      files: args.slice(1),
+      option: firstUserArg[1],
+      count: +firstUserArg.substr(2)
+    };
   }
 
-  if (isFinite(parseInt(option[1]))) {
-    return { files: args.slice(1), option: "n", count: option.substr(1) };
+  if (isFinite(parseInt(firstUserArg[1]))) {
+    return { files: args.slice(1), option: "n", count: firstUserArg.substr(1) };
   }
-  return { files: args.slice(2), option: option[1], count: +args[1] };
+  return { files: args.slice(2), option: firstUserArg[1], count: +args[1] };
 };
 
 exports.extractUserArgs = extractUserArgs;
