844412d30df6212ce968b8bdd234ab9662956a83
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2018-12-18 12:10:14 +0530

    Renaming variable validatedFiles --> validatedArgs.

--- src/parseInput.js
+++ src/parseInput.js
@@ -1,6 +1,6 @@
 const extractUserOptions = function (args) {
   let option = args[0];
-  let validatedFiles = { files: [] };
+  let validatedArgs = { files: [] };
   if (!option.startsWith("-")) {
     return [{ files: [], n: 10 }, args];
   }
@@ -10,23 +10,23 @@ const extractUserOptions = function (args) {
   }
 
   if (!isFinite(option.substr(1)) && +option.substr(2)) {
-    validatedFiles[option[1]] = +option.substr(2);
-    return [validatedFiles, args.slice(1)];
+    validatedArgs[option[1]] = +option.substr(2);
+    return [validatedArgs, args.slice(1)];
   }
 
   if (isFinite(parseInt(option[1]))) {
     return [{ files: [], n: option.substr(1) }, args.slice(1)];
   }
-  validatedFiles[option[1]] = args[1];
-  return [validatedFiles, args.slice(2)];
+  validatedArgs[option[1]] = args[1];
+  return [validatedArgs, args.slice(2)];
 };
 
 const extractUserArgs = function (args) {
   let extractOption = extractUserOptions(args);
-  let validatedFiles = extractOption[0];
+  let validatedArgs = extractOption[0];
   userArgs = extractOption[1];
-  validatedFiles["files"] = userArgs;
-  return validatedFiles;
+  validatedArgs["files"] = userArgs;
+  return validatedArgs;
 
 };
 exports.extractUserArgs = extractUserArgs;
